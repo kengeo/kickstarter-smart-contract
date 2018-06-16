5cbf078201cecbcc16414c79af17a667110127ae
Max Li <maxlivinci@gmail.com>
2018-06-16 14:49:43 +0800

    testing warmup

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -6,7 +6,7 @@ const web3 = new Web3(ganache.provider());
 const compiledFactory = require('../ethereum/build/CampaignFactory.json');
 const compiledCampaign = require('../ethereum/build/Campaign.json');
 
-describe('Inbox', () => {
+describe('Inbox', async () => {
   let accounts;
   let factory;
   let campaignAddress;
@@ -18,18 +18,21 @@ describe('Inbox', () => {
     factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
       .deploy({ data: compiledFactory.bytecode })
       .send({ from: accounts[0], gas: '1000000' });
-    });
-
+  
     await factory.methods.createCampaign('100').send({
       from: accounts[0],
       gas: '1000000'
     });
-
+  
     [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
-
     campaign = await new web3.eth.Contract(
       JSON.parse(compiledCampaign.interface),
       campaignAddress
     );
-  })
+  });
+
+  it('deploys a factory and a campaign', () => {
+    expect(factory.options.address).toBeTruthy()
+    expect(campaign.options.address).toBeTruthy()
+  });
 });
